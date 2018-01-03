bae420fe7e27e561c78d88a750775deee3d17e65
sonwonrak92 <sonwonrak@gmail.com>
2018-01-03 09:13:46 +0900

    second commit

--- ocr.py
+++ ocr.py
@@ -2,11 +2,10 @@ import pytesseract
 from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
+#tesseract.exe 저장 위치
 
-# Include the above line, if you don't have tesseract executable in your PATH
-# Example tesseract_cmd: 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract'
 
-im = Image.open('d.png')
-text = pytesseract.image_to_string(im, lang = 'kor')
+im = Image.open('d.png') #인식사진위치
+text = pytesseract.image_to_string(im, lang = 'kor') #lang설정
 
 print(text)
